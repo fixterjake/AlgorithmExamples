c97e79ca96db0b5cab8b75138a356383c4b7c9fa
Jacob Boyles <fixterjake@gmail.com>
2019-08-29 22:28:15 -0400

    Implementation of bubble sort with lots of base app changes for scaling to more algorithms

--- src/main/java/com/fixterjake/testing/App.java
+++ src/main/java/com/fixterjake/testing/App.java
@@ -5,39 +5,75 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
     /** File holding random integers **/
-    private final String filePath = "nums.txt";
+    private static final String numsPath = "nums.txt";
+    /** File holding result of sorting **/
+    private static final String resultsPath = "results.txt";
     /** Number of integers to generate **/
-    private final int fileSize = 100000;
+    private static final int fileSize = 100000;
     /** List to populate and sort using given algorithms **/
-    private ArrayList<Integer> list;
+    private static ArrayList<Integer> list = new ArrayList<Integer>();;
+    /** List of execution times **/
+    private static ArrayList<Long> executionTimes = new ArrayList<Long>();
+    /** List of algorithm types tested **/
+    private static ArrayList<String> algorithmTypes = new ArrayList<String>();
 
-    public App() {
-        if (list.size() <= 0) {
-            populateList();
+    /**
+     * Write execution times to result file
+     */
+    protected static void writeResult() {
+        try {
+            PrintWriter pout = new PrintWriter(new File(resultsPath));
+            pout.println("|| ****************************** ||");
+            pout.println("|| Execution times for each algorithm:");
+            for (int i = 0; i < executionTimes.size(); i++) {
+                pout.println("|| " + algorithmTypes.get(i) + ": "
+                                   + executionTimes.get(i) + " milliseconds, "
+                                   + TimeUnit.MILLISECONDS.toSeconds(executionTimes.get(i)) + " seconds");
+            }
+            pout.println("|| ****************************** ||");
+            pout.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private void populateList() {
-
+    /**
+     * Function to read integers from file and populate list.
+     */
+    private static void populateList() {
+        try {
+            Scanner sc = new Scanner(new File(numsPath));
+            while (sc.hasNext()) {
+                list.add(sc.nextInt());
+            }
+            sc.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Initialize file with n number of random integers
      * n being a global variable.
      */
-    private void init() {
+    private static void init() {
         Random random = new Random();
-        File file = new File(filePath);
         try {
-            PrintWriter pout = new PrintWriter(file);
+            PrintWriter pout = new PrintWriter(new File(numsPath));
             for (int i = 0; i < fileSize; i++) {
                 pout.println(random.nextInt(fileSize));
             }
             populateList();
+            pout = new PrintWriter(new File(resultsPath));
+            pout.println("|| ****************************** ||");
+            pout.println("|| Results of algorithms:");
             pout.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,9 +81,18 @@ public class App {
     }
 
     /**
+     * Converts nanoseconds to milliseconds for logging & output
+     * @param time Execution time in nanoseconds
+     * @return Execution time in milliseconds
+     */
+    private static long processTime(long time) {
+        return time / 1000000;
+    }
+
+    /**
      * Helper function to print spacer.
      */
-    private static void printSpacer() {
+    protected static void printSpacer() {
         System.out.println("|| ****************************** ||");
     }
 
@@ -55,7 +100,7 @@ public class App {
      * Helper function to print given string.
      * @param string String to print
      */
-    private static void print(String string) {
+    protected static void print(String string) {
         System.out.println("|| " + string);
     }
 
@@ -70,9 +115,11 @@ public class App {
         }
         printSpacer();
         print("Available algorithms, please enter the corresponding number: ");
-        print("1 : Bubble sort");
         printSpacer();
-        int choice;
+        print("1 : Bubble sort");
+        print("0 : Exit");
+        printSpacer();
+        int choice = 0;
         try {
             choice = sc.nextInt();
         }
@@ -82,5 +129,21 @@ public class App {
             printSpacer();
             System.exit(1);
         }
+        switch (choice) {
+            case 1:     long time = processTime(BubbleSort.bubbleSort(list));
+                        printSpacer();
+                        print("Execution time (milliseconds): " + time);
+                        print("Now exiting");
+                        printSpacer();
+                        executionTimes.add(time);
+                        algorithmTypes.add("Bubble Sort");
+                        break;
+
+            default:    printSpacer();
+                        print("Exiting.");
+                        printSpacer();
+        }
+        writeResult();
+        System.exit(0);
     }
 }
--- src/main/java/com/fixterjake/testing/BubbleSort.java
+++ src/main/java/com/fixterjake/testing/BubbleSort.java
@@ -5,7 +5,19 @@ import java.util.ArrayList;
 
 public class BubbleSort {
 
-    public static void bubbleSort(ArrayList<Integer> list) {
-
+    public static long bubbleSort(ArrayList<Integer> list) {
+        long startTime = System.nanoTime();
+        int temp;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 1; j < list.size() - i; j++) {
+                if (list.get(j - 1) > list.get(j)) {
+                    temp = list.get(j - 1);
+                    list.set(j - 1, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        long endTime = System.nanoTime();
+        return endTime - startTime;
     }
 }
