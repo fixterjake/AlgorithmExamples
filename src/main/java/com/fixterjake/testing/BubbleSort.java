4ae8a08f1ad3e3c1049e65184aff3d87f0abb0a4
Jacob Boyles <fixterjake@gmail.com>
2019-08-29 20:53:07 -0400

    Added maven support

---
+++ src/main/java/com/fixterjake/testing/BubbleSort.java
@@ -0,0 +1,11 @@
+package com.fixterjake.testing;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class BubbleSort {
+
+    public static void bubbleSort(ArrayList<Integer> list) {
+
+    }
+}
